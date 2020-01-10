0da3139cd272127b489089ecc906700b7e51aa56
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-01-10 12:31:15 +0700

    added splashscreen

---
+++ app/src/main/java/byc/avt/avanteeborrower/intro/OnBoardingActivity.java
@@ -0,0 +1,16 @@
+package byc.avt.avanteeborrower.intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import byc.avt.avanteeborrower.R;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
---
+++ app/src/main/java/byc/avt/avanteeborrower/intro/SplashActivity.java
@@ -0,0 +1,27 @@
+package byc.avt.avanteeborrower.intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import byc.avt.avanteeborrower.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent onBoard = new Intent(SplashActivity.this, OnBoardingActivity.class);
+                startActivity(onBoard);
+                finish();
+            }
+        }, 2000);
+    }
+}
