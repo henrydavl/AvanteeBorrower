9852f873e14915f070df54388e95983364fb3986
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-01-28 13:02:57 +0700

    Onboarding 90%

--- app/src/main/java/byc/avt/avanteeborrower/intro/OnBoardingActivity.java
+++ app/src/main/java/byc/avt/avanteeborrower/intro/OnBoardingActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import byc.avt.avanteeborrower.R;
@@ -11,20 +15,72 @@ import byc.avt.avanteeborrower.adapter.OnBoardAdapter;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
-    private ViewPager onBoardPager;
-    private TextView[] dots;
-    private OnBoardAdapter onBoardAdapter;
+    private LinearLayout onBoardDots;
+    private Button btnSkip;
+    private String[] board_heading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
-        onBoardPager = findViewById(R.id.onBoardPager);
-        onBoardAdapter = new OnBoardAdapter(this);
+        final ViewPager onBoardPager = findViewById(R.id.onBoardPager);
+        onBoardDots = findViewById(R.id.onBoardDots);
+        btnSkip = findViewById(R.id.btnSkip);
+        board_heading = getResources().getStringArray(R.array.onboard_heading);
+        OnBoardAdapter onBoardAdapter = new OnBoardAdapter(this);
         onBoardPager.setAdapter(onBoardAdapter);
+        dotsIndicator(0);
+        onBoardPager.addOnPageChangeListener(listener);
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBoardPager.setCurrentItem(board_heading.length);
+            }
+        });
     }
 
-    public void dotsIndicator() {
+    public void dotsIndicator(int position) {
+        TextView[] dots = new TextView[board_heading.length];
+        onBoardDots.removeAllViews();
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(8,0,8,0);
 
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(32);
+            dots[i].setLayoutParams(params);
+            dots[i].setTextColor(getResources().getColor(R.color.neutral));
+            onBoardDots.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.colorPrimary));
+        }
     }
+
+    ViewPager.OnPageChangeListener listener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            dotsIndicator(position);
+
+            if (position == board_heading.length - 1) {
+                btnSkip.setEnabled(false);
+                btnSkip.setVisibility(View.INVISIBLE);
+            } else {
+                btnSkip.setEnabled(true);
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
