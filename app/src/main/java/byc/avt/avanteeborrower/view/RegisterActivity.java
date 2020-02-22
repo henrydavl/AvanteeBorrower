e8747a3d472b5fa9b4a62f07e60a9ed682e90f67
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-02-23 01:43:46 +0700

    Added some validation on registed

--- app/src/main/java/byc/avt/avanteeborrower/view/RegisterActivity.java
+++ app/src/main/java/byc/avt/avanteeborrower/view/RegisterActivity.java
@@ -3,11 +3,15 @@ package byc.avt.avanteeborrower.view;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
@@ -16,6 +20,9 @@ import androidx.lifecycle.ViewModelProviders;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Pattern;
 
 import byc.avt.avanteeborrower.R;
 import byc.avt.avanteeborrower.model.User;
@@ -29,6 +36,17 @@ public class RegisterActivity extends AppCompatActivity {
     private Button btnRegister;
     private CheckBox checkAgree;
     private AuthenticationViewModel viewModel;
+    private Boolean checkInput = false, readTerm = false;
+    private Timer timer;
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+            "(?=.*[0-9])" +         //at least has 1 number
+            "(?=.*[a-z])" +         //at least has 1 lower case letter
+            "(?=.*[A-Z])" +         //at least has 1 upper case letter
+            //"(?=.*[a-zA-Z])" + //can be any letter (uppercase/lowercase)
+            "(?=.*[@#$%^&+=])" + //at least 1 special character
+            "(?=\\\\S+$)." + //no white spaces
+            "{8,}" + //at least 8 character
+            "$");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +67,21 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Objects.requireNonNull(editPhoneNumber.getEditText()).addTextChangedListener(registerTextWatcher);
         Objects.requireNonNull(editPassword.getEditText()).addTextChangedListener(registerTextWatcher);
-        checkAgree.setOnClickListener(showTermListener);
+        checkAgree.setOnCheckedChangeListener(showTermListener);
     }
 
-    private View.OnClickListener showTermListener = new View.OnClickListener() {
+
+    private CompoundButton.OnCheckedChangeListener showTermListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
-        public void onClick(View view) {
-            TermFragment termFragment = TermFragment.getInstance();
-            termFragment.show(getSupportFragmentManager(), termFragment.getTag());
+        public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+            if (isChecked && TermFragment.read) {
+                checkAgree.setChecked(true);
+                readTerm = TermFragment.read;
+            } else {
+                TermFragment termFragment = TermFragment.getInstance();
+                termFragment.show(getSupportFragmentManager(), termFragment.getTag());
+                checkAgree.setChecked(false);
+            }
         }
     };
 
@@ -69,33 +94,91 @@ public class RegisterActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            phoneNumber = Objects.requireNonNull(editPhoneNumber.getEditText()).getText().toString().trim();
-            password = Objects.requireNonNull(editPassword.getEditText()).getText().toString().trim();
-            rePassword = Objects.requireNonNull(edtConfirmPassword.getEditText()).getText().toString().trim();
-            btnRegister.setEnabled(!phoneNumber.isEmpty() && !password.isEmpty() && !rePassword.isEmpty());
+            String phoneNum = editPhoneNumber.getEditText().getText().toString().trim();
+            String pass = editPassword.getEditText().getText().toString().trim();
+            String rePass = edtConfirmPassword.getEditText().getText().toString().trim();
+            checkInput = !phoneNum.isEmpty() && !pass.isEmpty() && !rePass.isEmpty();
+//            if (checkInput && readTerm){
+//                btnRegister.setEnabled(true);
+//            } else {
+//                btnRegister.setEnabled(false);
+//            }
+//            if (timer != null){
+//                timer.cancel();
+//            }
         }
 
         @Override
         public void afterTextChanged(Editable editable) {
-
+//            timer = new Timer();
+//            timer.schedule(new TimerTask() {
+//                @Override
+//                public void run() {
+//                    String phoneNum = editPhoneNumber.getEditText().getText().toString().trim();
+//                    String pass = editPassword.getEditText().getText().toString().trim();
+//                    String rePass = edtConfirmPassword.getEditText().getText().toString().trim();
+//                    checkInput = !phoneNum.isEmpty() && !pass.isEmpty() && !rePass.isEmpty();
+//                }
+//            }, 100);
+            if (checkInput && readTerm){
+                btnRegister.setEnabled(true);
+            } else {
+                btnRegister.setEnabled(false);
+            }
         }
     };
 
     private boolean checkPassword(){
+        password = editPassword.getEditText().getText().toString().trim();
+        rePassword = edtConfirmPassword.getEditText().getText().toString().trim();
         if (rePassword.equals(password)){
             edtConfirmPassword.setError(null);
             return true;
+//            if (PASSWORD_PATTERN.matcher(password).matches()){
+//                editPassword.setError(null);
+//                return true;
+//            } else {
+//                editPassword.setError("Password terlalu lemah");
+//                return false;
+//            }
         } else {
+            edtConfirmPassword.setError("Kata Sandi Tidak Sama");
             return false;
         }
     }
 
-    public void confirmInput(View v) {
-        if (!checkPassword()) {
-            edtConfirmPassword.setError("Kata Sandi Tidak Sama");
+    private boolean validateEmail(String email){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edtEmail.setError("Email tidak valid");
+            return false;
         } else {
-            String email = Objects.requireNonNull(edtEmail.getEditText()).getText().toString().trim();
-            String ref_id = Objects.requireNonNull(edtRefId.getEditText()).getText().toString().trim();
+            edtEmail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePhoneNumber(String phone){
+        if (phone.length() < 12){
+            editPhoneNumber.setError("No. Telepon tidak valid");
+            return false;
+        } else {
+            editPhoneNumber.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmInput(View v) {
+        phoneNumber = editPhoneNumber.getEditText().getText().toString().trim();
+        String email = Objects.requireNonNull(edtEmail.getEditText()).getText().toString().trim();
+        String ref_id = Objects.requireNonNull(edtRefId.getEditText()).getText().toString().trim();
+        if (!email.isEmpty()){
+            checkPassword();
+            validatePhoneNumber(phoneNumber);
+            validateEmail(email);
+        } else {
+            if (!checkPassword() | !validatePhoneNumber(phoneNumber)) {
+                return;
+            }
             // POST to server through endpoint
             User user = new User();
             user.setEmail(email);
@@ -112,7 +195,6 @@ public class RegisterActivity extends AppCompatActivity {
             if (aBoolean) {
                 showMessage("Success");
                 //intent to sms verification
-
             } else {
                 showMessage("failed");
             }
@@ -121,5 +203,18 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void showMessage(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            if (TermFragment.getInstance().isVisible()){
+                TermFragment.getInstance().dismiss();
+            } else {
+                onBackPressed();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/byc/avt/avanteeborrower/view/misc/TermFragment.java
+++ app/src/main/java/byc/avt/avanteeborrower/view/misc/TermFragment.java
@@ -1,30 +1,42 @@
 package byc.avt.avanteeborrower.view.misc;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.appcompat.widget.Toolbar;
-
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.Objects;
 
 import byc.avt.avanteeborrower.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TermFragment extends BottomSheetDialogFragment {
+public class TermFragment extends BottomSheetDialogFragment{
 
+    @SuppressLint("StaticFieldLeak")
     private static TermFragment instance;
+    public static Boolean read = false;
+    private Button btnCancel, btnNext;
+    private CheckBox cbRead;
 
     public TermFragment() {
-        // Required empty public constructor
+
     }
 
     public static TermFragment getInstance() {
@@ -45,5 +57,63 @@ public class TermFragment extends BottomSheetDialogFragment {
         super.onViewCreated(view, savedInstanceState);
         Toolbar toolbar = view.findViewById(R.id.term_toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_close);
+        ((AppCompatActivity) Objects.requireNonNull(getActivity())).setSupportActionBar(toolbar);
+        Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        NestedScrollView nestedScrollView = view.findViewById(R.id.nested_scroll_term);
+        btnCancel = view.findViewById(R.id.btn_term_cancel);
+        btnNext = view.findViewById(R.id.btn_term_next);
+        cbRead = view.findViewById(R.id.cb_term_read);
+
+        nestedScrollView.setOnScrollChangeListener(readTermListener);
+        cbRead.setOnCheckedChangeListener(cbReadCheckedListener);
+        btnNext.setOnClickListener(onNext);
+        btnCancel.setOnClickListener(onCancel);
+    }
+
+    private NestedScrollView.OnScrollChangeListener readTermListener = new NestedScrollView.OnScrollChangeListener() {
+        @Override
+        public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+            if (scrollY == (v.getChildAt(0).getMeasuredHeight()) - v.getMeasuredHeight()){
+                cbRead.setEnabled(true);
+            }
+        }
+    };
+
+    private CompoundButton.OnCheckedChangeListener cbReadCheckedListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+            if (checked){
+                btnCancel.setEnabled(true);
+                btnNext.setEnabled(true);
+            } else {
+                btnCancel.setEnabled(false);
+                btnNext.setEnabled(false);
+            }
+        }
+    };
+
+    private View.OnClickListener onNext = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            read = true;
+            instance.dismiss();
+        }
+    };
+
+    private View.OnClickListener onCancel = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            instance.dismiss();
+        }
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home && instance.isVisible()) {
+            instance.dismiss();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
